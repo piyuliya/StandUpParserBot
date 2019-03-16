416a2437611668964912f5c98dfba7695d5dea7c
Yuliua Pak <3511521@gmail.com>
2019-03-17 00:53:33 +0300

    First commit

---
+++ standupparserbot.py
@@ -0,0 +1,33 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
+
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    mybot = Updater(settings.token, request_kwargs=PROXY)
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    mybot.start_polling()
+    mybot.idle()
+
+def greet_user(bot, update):
+    text = 'вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+main()
+
