74076c2f3d8d5d851bed4bf7ddc94533af84af43
Yuliua Pak <3511521@gmail.com>
2019-03-23 21:17:01 +0300

    added perser

---
+++ standup_html.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    try:
+        result = requests.get(url)
+        result.raise_for_status()
+        return result.text
+    except(requests.RequestExeption, ValueError):
+        return False
+
+
+def get_event(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    all_event = soup.findAll('div', class_="t778__wrapper no-underline")
+    for event in all_event:
+        data_event = event.find(
+            'div',
+            class_="t778__descr t-descr t-descr_xxs no-underline").text
+        print(data_event)
+
+
+if __name__ == '__main__':
+    page = 1
+    while True:
+        html = get_html('https://standupstore.ru/page/%s' % page)
+        if html:
+            get_event(html)
+            with open('standup.html', 'w', encoding='utf8') as f:
+                f.write(html)
+        page += 1
