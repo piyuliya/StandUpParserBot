d5a72d962f75c29550c3354b158b67b66a6484a5
Yuliua Pak <3511521@gmail.com>
2019-03-28 17:46:43 +0300

    Доделан парсер, собирает в список

--- standup_html.py
+++ standup_html.py
@@ -14,16 +14,33 @@ def get_html(url):
 def get_event(html):
     soup = BeautifulSoup(html, 'html.parser')
     all_event = soup.findAll('div', class_="t778__wrapper no-underline")
+    result_event = []
     for event in all_event:
         data_event = event.find(
             'div',
             class_="t778__descr t-descr t-descr_xxs no-underline").text
-        print(data_event)
+        price_event = event.find(
+            'div',
+            class_="t778__price t778__price-item t-name t-name_xs").text
+        availability = event.find(
+            'div',
+            class_="t778__imgwrapper").text
+        url = event.find(
+            'div',
+            class_="t778__bgimg t778__bgimg_first_hover t-bgimg js-product-img")['data-original']
+        result_event.append({
+            'data_event': data_event,
+            'price_event': price_event,
+            'availability': availability,
+            'url': url
+            })
+        print(result_event)
+    return result_event
 
 
 def pages(html):
     page = 1
-    while page < 6:
+    while page < 5:
         html = get_html('https://standupstore.ru/page/%s' % page)
         if html:
             get_event(html)
