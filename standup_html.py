17d60904202bf4d297df9c4e32814abeea24f50c
Yuliua Pak <3511521@gmail.com>
2019-04-01 19:38:37 +0300

    попытка добавить базу данных

--- standup_html.py
+++ standup_html.py
@@ -1,6 +1,35 @@
+import os
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 import requests
 from bs4 import BeautifulSoup
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+engine = create_engine('sqlite:///' + os.path.join(basedir, 'event.db'))
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base.metadata.create_all(engine)
+
+class Events(Base):
+        id = Column(Integer, primary_key=True)
+        data_event = Column(DateTime, nullable=False)
+        price_event = Column(String, nullable=True)
+        availability = Column(String, nullable=True)
+        url = Column(String, unique=True, nullable=False)
+
+        def __init__(self, data_event, price_event, availability, url):
+            self.data_event = data_event
+            self.price_event = price_event
+            self.availability = availability
+            self.url = url
+
+        def __repr__(self):
+            return '<Events {} {} {}>'.format(self.data_event, self.price_event, self.url)
 
 def get_html(url):
     try:
@@ -14,11 +43,14 @@ def get_html(url):
 def get_event(html):
     soup = BeautifulSoup(html, 'html.parser')
     all_event = soup.findAll('div', class_="t778__wrapper no-underline")
-    result_event = []
     for event in all_event:
         data_event = event.find(
             'div',
             class_="t778__descr t-descr t-descr_xxs no-underline").text
+        try:
+            data_event = datetime.strptime(data_event, '%d %B, %H:%M')
+        except(ValueError):
+            data_event = datetime.now()
         price_event = event.find(
             'div',
             class_="t778__price t778__price-item t-name t-name_xs").text
@@ -28,14 +60,7 @@ def get_event(html):
         url = event.find(
             'div',
             class_="t778__bgimg t778__bgimg_first_hover t-bgimg js-product-img")['data-original']
-        result_event.append({
-            'data_event': data_event,
-            'price_event': price_event,
-            'availability': availability,
-            'url': url
-            })
-        print(result_event)
-    return result_event
+        save_events(data_event, price_event, availability, url)
 
 
 def pages(html):
@@ -55,5 +80,12 @@ def check_stand_up_site_page():
         pages(html)
 
 
+def save_events(data_event, price_event, availability, url):
+    new_event = Events(data_event=data_event, price_event=price_event, availability=availability, url=url)
+    session.add(new_event)
+    session.commit()
+
+
 if __name__ == '__main__':
     check_stand_up_site_page()
+   
