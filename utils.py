ce7baa7b893010bdf27690ca39b8274e4db232e7
Simon Osipov <osipov.simon@gmail.com>
2019-04-16 22:16:31 +0300

    Added comments

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from telegram import ReplyKeyboardMarkup
 
 
 def get_keyboard():
-    my_keyboard = ReplyKeyboardMarkup([
+    my_keyboard = ReplyKeyboardMarkup([ # Обрати внимание на PEP8, квадратные скобки можно расположить поудачней)
         ['Посмотреть афишу', 'Подписаться на обновления']
         ], resize_keyboard=True
         )
