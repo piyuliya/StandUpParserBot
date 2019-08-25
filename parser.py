065fc67a90abe110562e85590ca6229075a5d3fc
Yuliua Pak <3511521@gmail.com>
2019-08-25 17:27:05 +0300

    пагинация

--- parser.py
+++ parser.py
@@ -27,7 +27,7 @@ def get_html(url):
 
 
 def parse_pages(html):
-    for page in range(1, 5):
+    for page in range(1, 7):
         html = get_html(f'https://standupstore.ru/page/{page}')
         if html:
             get_event(html)
